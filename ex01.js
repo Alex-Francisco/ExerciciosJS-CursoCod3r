f84818e4b14aa34449b21a47e41328be23dbe690
Alex Francisco <alex2007f@gmail.com>
2021-09-08 18:54:37 -0300

    exercício 01 resolvido

--- ex01.js
+++ ex01.js
@@ -1,4 +1,10 @@
 /* Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
 multiplicação e divisão desses valores. */
 
-
+function execOpsAritmeticas(valor1, valor2) {
+  console.log(`Operação de soma: ${valor1} + ${valor2} = ${valor1 + valor2}`)
+  console.log(`Operação de subtração: ${valor1} - ${valor2} = ${valor1 - valor2}`)
+  console.log(`Operação de multiplicação: ${valor1} x ${valor2} = ${valor1 * valor2}`)
+  console.log(`Operação de divisão: ${valor1} ÷ ${valor2} = ${valor1 / valor2}`)
+}
+execOpsAritmeticas(10, 20);
