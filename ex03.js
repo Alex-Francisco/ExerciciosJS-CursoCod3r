0a73cf05e9c09bb2b7204c37803f7d79d6422d36
Alex Francisco <alex2007f@gmail.com>
2021-09-08 19:18:38 -0300

    Exercício 03 resolvido

---
+++ ex03.js
@@ -0,0 +1,7 @@
+/* Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente */
+
+function calcExponeciacao(base, expoente) {
+  console.log(`${base} elevado a ${expoente} é = ${base ** expoente}`)
+}
+
+calcExponeciacao(10, 2);
