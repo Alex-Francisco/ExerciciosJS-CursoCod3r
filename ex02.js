45e3baf4dbad6b3799f8ad2c79561f0754511e78
Alex Francisco <alex2007f@gmail.com>
2021-09-09 17:58:18 -0300

    Exercício 02 Resolvido

--- ex02.js
+++ ex02.js
@@ -7,10 +7,19 @@ triângulo). */
 function ladosTriangulo (ab, cd, ef) {
   if (ab + cd > ef && ab + ef > cd && ef + cd > ab) {
         console.log("Ok! Com essas medidas é possível formar um triângulo.");
+        if (ab == cd && cd == ef) {
+          console.log("O triângulo formado é Equilátero.");
+        } else if (ab != cd && cd != ef && ef != ab) {
+          console.log("O triângulo formado é Escaleno");
+        } else {
+          console.log("O triângulo formado é Isósceles");
+        }
       } else
         console.log("Negativo! Não se pode formar um triângulo com essas medidas.");
 }
 
+ladosTriangulo(5, 8, 7);
 ladosTriangulo(16, 20, 30);
+ladosTriangulo(15, 15, 15);
 ladosTriangulo(20, 15, 5);
 ladosTriangulo(10, 25, 13);
