7a966bbafd1e5413ef4a2482817902a0b516f803
Alex Francisco <alex2007f@gmail.com>
2021-09-10 15:23:02 -0300

    Exercício 08 Resolvido

---
+++ ex08.js
@@ -0,0 +1,33 @@
+/* ​Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
+mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo).Obs.: O primeiro jogo não conta como novo recorde do melhor.
+Exemplo:
+String: “10 20 20 8 25 3 0 30 1”
+Retorno: [3, 7] (Significa que ele bateu três vezes seu recorde de melhor pontuação e a pior pontuação
+aconteceu no sétimo jogo.) */
+
+let pontuacao = "10 20 20 8 25 3 0 30 1"
+
+function checarPontuacao(pontuacao) {
+    let pontosFeito = pontuacao.split(" ");
+    let qtdRecord = 1;
+    let piorPontuacao = 1;
+    let maiorPontuacao = pontosFeito[0];
+    let menorPontuacao = pontosFeito[0];
+    
+    for (posicaoValor = 0; posicaoValor < pontosFeito.length; posicaoValor++) {
+        
+        if (pontosFeito[posicaoValor] > maiorPontuacao) {
+            maiorPontuacao = pontosFeito[posicaoValor]
+            qtdRecord++
+        } else if (pontosFeito[posicaoValor] < menorPontuacao){
+            menorPontuacao = pontosFeito[posicaoValor]
+            piorPontuacao = posicaoValor + 1 
+        }
+    }
+    return [qtdRecord, piorPontuacao]
+}
+console.log(checarPontuacao(pontuacao))
