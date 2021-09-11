40778a34347791540b357b19f37f5fb832a6c8f2
Alex Francisco <alex2007f@gmail.com>
2021-09-11 10:33:19 -0300

    Exercício 11 Resolvido

--- ex11.js
+++ ex11.js
@@ -1,4 +1,24 @@
-/* Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
-multiplicação e divisão desses valores. */
+/* As regras para o cálculo dos anos bissextos são as seguintes:
+De 4 em 4 anos é ano bissexto;
+De 100 em 100 anos não é ano bissexto;
+De 400 em 400 anos é ano bissexto;
+Prevalecem as últimas regras sobre as primeiras.
+Partindo daí elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a
+mensagem e retornando true ou false. */
 
+function anoBissexto(ano) {
+    if (ano % 400 == 0) {
+        return true
+    } else if (ano % 100 == 0) {
+        return false
+    } else if (ano % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
+console.log(anoBissexto(1700));
+console.log(anoBissexto(2020));
+console.log(anoBissexto(1900));
+console.log(anoBissexto(2024));
