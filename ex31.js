2cb31d5d214bba6f1aac43e4d1adeb5364edbfc9
Alex Francisco <alex2007f@gmail.com>
2021-09-29 10:46:10 -0300

    Exercício 31 Resolvido

--- ex31.js
+++ ex31.js
@@ -1,4 +1,16 @@
-/* Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
-multiplicação e divisão desses valores. */
+/* Escrever um algoritmo que percorre um vetor de inteiros, conta quantos números negativos há nesse vetor
+e imprime a quantidade no console. */
 
+vetor = [-20, 2, -5, 15, 27, -6, -202, 105, 55, -10];
 
+function numerosNegativos(vetorPassado) {
+    qtdNegativo = 0;
+    for (i in vetorPassado) {
+        if (vetorPassado[i] < 0) {
+            qtdNegativo++;
+        }
+    }
+    console.log(`A quantidade de números negativos é igual a -> ${qtdNegativo}`);
+}
+
+numerosNegativos(vetor);
