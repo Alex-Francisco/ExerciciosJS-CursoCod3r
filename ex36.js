0338adb04bf212fb000e8bd06e9edc5c77f4a991
Alex Francisco <alex2007f@gmail.com>
2021-09-30 16:08:54 -0300

    Exercício 36 Resolvido

---
+++ ex36.js
@@ -0,0 +1,27 @@
+/* Crie duas funções que recebem dois parâmetros, um vetor com apenas valores numéricos e um número
+inteiro. Faça com que a primeira função retorne outro vetor que será resultado da multiplicação de cada
+elemento pelo número passado como parâmetro. A segunda função fará o mesmo da primeira se e somente se
+o valor do elemento for maior que 5. */
+
+function calcMult(vetorPassado, numPassado) {
+    let resultado = []
+    for (let i = 0; i < vetorPassado.length; i++) {
+        resultado.push(vetorPassado[i] * numPassado);
+    }
+    console.log(resultado);
+}
+
+function calcMult2(vetorPassado, numPassado) {
+    if (numPassado > 5) {
+        let resultado = []
+        for( let i = 0; i < vetorPassado.length; i++) {
+            resultado.push(vetorPassado[i] * numPassado);
+        }
+    console.log(resultado);
+    }
+}
+
+vetor = [1, 5, 10, 50, 100];
+calcMult(vetor, 5);
+calcMult2(vetor, 4);
+calcMult2(vetor, 10);
