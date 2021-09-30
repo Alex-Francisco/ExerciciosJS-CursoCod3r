0c34abf681f026ca896e85f80171618013a47d36
Alex Francisco <alex2007f@gmail.com>
2021-09-30 11:51:53 -0300

    Exercício 34 Resolvido

---
+++ ex34.js
@@ -0,0 +1,27 @@
+/* Construa uma função que receberá duas Strings de tamanhos variados e que retornará True ou False caso
+todos os caracteres (independentemente de ser maiúsculo ou minúsculo) estejam contidos em ambas palavras. */
+
+function avaliaStrig(str1, str2) {
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    let existeChar = null;
+
+    for (let i = 0; i < str1.length; i++) {
+
+        for (let j = 0; j < str2.length; j++) {
+            if(str1[i] == str2[i]) {
+                existeChar = true            
+            } else {
+                existeChar = false;
+            }
+        }
+    }
+    if (existeChar) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+avaliaStrig("dois", "dois");
+avaliaStrig("Meu", "mel");
